5df99e6f7b737ece37d86b61baf43f6e3924a218
Paulo <liedtke.paulo@gmail.com>
2021-11-10 17:23:59 +0200

    main.py added

---
+++ main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title('Crypto-Currency price prediction models')
+st.write('This small app is part of an essay written for an application to the master programm computer science of vrije universiteit amsterdam and university of amsterdam.')
+
+st.write('A link to this essay can be found here')
+st.write('[Comparing different approaches to predict the price of cryptocurrency and their implementation in python](https://docs.google.com/document/d/1F-qxHIrgdxImiHJMIApfMN1o5ZctGRLMLM61VOv7ZCE/edit?usp=sharing)')
