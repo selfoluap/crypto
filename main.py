435421fbb915f168d2f765e1ad114f837bb8c96b
Paulo <liedtke.paulo@gmail.com>
2021-11-10 18:00:30 +0200

    secret test

--- main.py
+++ main.py
@@ -5,3 +5,4 @@ st.write('This small app is part of an essay written for an application to the m
 
 st.write('A link to this essay can be found here')
 st.write('[Comparing different approaches to predict the price of cryptocurrency and their implementation in python](https://docs.google.com/document/d/1F-qxHIrgdxImiHJMIApfMN1o5ZctGRLMLM61VOv7ZCE/edit?usp=sharing)')
+st.write(st.secrets["test"])
